751592bd30cef888818edbe5f5c07ad4cdd397db
Sebastian Michaelsen <sebastian@app-zap.de>
2014-10-15 16:19:43 +0200

    Output PHPFramework version in HTML comment

--- classes/SignalSlot/CoreSlots.php
+++ classes/SignalSlot/CoreSlots.php
@@ -7,7 +7,7 @@ class CoreSlots {
 
   public static function addFrameworkSignatureToOutput(&$output) {
     if (Configuration::get('phpframework', 'powered_by', TRUE)) {
-      $output = preg_replace("/<head( (.)*?|)>/u", "<head$1>\n\t<!-- Powered by PHPFramework (https://github.com/app-zap/PHPFramework) -->", $output);
+      $output = preg_replace("/<head( (.)*?|)>/u", "<head$1>\n\t<!-- Powered by PHPFramework " . Configuration::get('phpframework', 'version', '') . " (https://github.com/app-zap/PHPFramework) -->", $output);
     }
   }
 
