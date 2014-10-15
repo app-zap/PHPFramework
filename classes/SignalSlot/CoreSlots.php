986501bf1af6f2b88432f56c4b0eb965dbc7dcd0
Sebastian Michaelsen <sebastian@app-zap.de>
2014-10-15 12:35:05 +0200

    Make inserting the PHPFramework html comment configurable

--- classes/SignalSlot/CoreSlots.php
+++ classes/SignalSlot/CoreSlots.php
@@ -1,10 +1,14 @@
 <?php
 namespace AppZap\PHPFramework\SignalSlot;
 
+use AppZap\PHPFramework\Configuration\Configuration;
+
 class CoreSlots {
 
-  public function addFrameworkSignatureToOutput(&$output) {
-    $output = preg_replace("/<head( (.)*?|)>/u", "<head$1>\n\t<!-- Powered by PHPFramework (https://github.com/app-zap/PHPFramework) -->", $output);
+  public static function addFrameworkSignatureToOutput(&$output) {
+    if (Configuration::get('phpframework', 'powered_by', TRUE)) {
+      $output = preg_replace("/<head( (.)*?|)>/u", "<head$1>\n\t<!-- Powered by PHPFramework (https://github.com/app-zap/PHPFramework) -->", $output);
+    }
   }
 
 }
