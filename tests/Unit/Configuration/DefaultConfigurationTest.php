c5e038087d05fefa3a07be2a07134e556e5451d1
Sebastian Michaelsen <sebastian@app-zap.de>
2014-11-21 16:14:13 +0100

    Improve automatic detection of project root directory

--- tests/Unit/Configuration/DefaultConfigurationTest.php
+++ tests/Unit/Configuration/DefaultConfigurationTest.php
@@ -8,7 +8,7 @@ use AppZap\PHPFramework\Configuration\DefaultConfiguration;
 class DefaultConfigurationTest extends \PHPUnit_Framework_TestCase {
 
   public function setUp() {
-    $_ENV['AppZap\PHPFramework\ProjectRoot'] = dirname(__FILE__);
+    $_ENV['AppZap\PHPFramework\ProjectRoot'] = __DIR__;
     Configuration::reset();
   }
 
