8de24213619ab12a5ee153c06ad8162489aca595
Sebastian Michaelsen <sebastian@app-zap.de>
2014-12-05 11:24:04 +0100

    [!!!] phpframework:project_root now always has a trailing slash

--- classes/Configuration/DefaultConfiguration.php
+++ classes/Configuration/DefaultConfiguration.php
@@ -11,20 +11,33 @@ class DefaultConfiguration {
    * @throws ApplicationPartMissingException
    */
   public static function initialize($application) {
-    $project_root = isset($_ENV['AppZap\PHPFramework\ProjectRoot']) ? $_ENV['AppZap\PHPFramework\ProjectRoot'] : getcwd();
-    $application_directory_path = $project_root . '/' . $application;
+    $projectRoot = self::getProjectRoot();
+    $application_directory_path = $projectRoot . $application;
     $application_directory = realpath($application_directory_path);
     if (!is_dir($application_directory)) {
       throw new ApplicationPartMissingException('Application folder ' . htmlspecialchars($application_directory_path) . ' not found', 1410538265);
     }
     $application_directory .= '/';
-    Configuration::set('phpframework', 'project_root', $project_root);
+    Configuration::set('phpframework', 'project_root', $projectRoot);
     Configuration::set('phpframework', 'db.migrator.directory', $application_directory . '_sql/');
     Configuration::set('application', 'application', $application);
     Configuration::set('application', 'application_directory', $application_directory);
     Configuration::set('application', 'routes_file', $application_directory . 'routes.php');
     Configuration::set('application', 'templates_directory', $application_directory . 'templates/');
     Configuration::set('phpframework', 'version', '1.4-dev');
+  }
+
+  /**
+   * @return string
+   */
+  protected static function getProjectRoot() {
+    if (isset($_ENV['AppZap\PHPFramework\ProjectRoot'])) {
+      $projectRoot = $_ENV['AppZap\PHPFramework\ProjectRoot'];
+    } else {
+      $projectRoot = getcwd();
+    }
+    $projectRoot = rtrim($projectRoot, '/') . '/';
+    return $projectRoot;
   }
 
 }
